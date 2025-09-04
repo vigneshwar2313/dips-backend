3f81dfa49d4839f8b39bd975ed788a2aba4d85c7
Vigneshwar2313 <vigneshwar.vivekanandan@shifteasy.com>
2025-09-04 12:28:40 +0530

    latest build

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,17 @@
 from flask import Flask
 from app.routes import bookings, services, staff, users
 from app.db.schema import init_db
+from flask_cors import CORS
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+front_end_urls = os.getenv('FRONT_END_URLS', '').split(',')
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, origins=front_end_urls)
 
     init_db()
 
